b7f055443599b35903bcfa255f4af11d31de5067
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-17 14:49:32 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,13 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      const isSatisfied = callback(this[i], i, this);
+      if (!isSatisfied) {
+        return isSatisfied;
+      }
+    }
+    return true;
   };
 }
 
