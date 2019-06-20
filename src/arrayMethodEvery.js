c9de8755eef6feed551a4251662dec5c7d876ece
OleksandrHlybchenko <logista@meta.ua>
2019-06-20 04:39:57 +0300

    for review

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,15 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this) === true) {
+        return true;
+      } else if (this[i] !== callback(this[i], i, this)) {
+        return false;
+      }
+    }
+
+    return true;
   };
 }
 
