498f2f070ee96816e6863d1debd5cfacbe2350d2
GitHubForDmitry <dmitrykoka@gmail.com>
2019-06-22 13:51:01 +0300

    sol

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,14 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
   };
 }
 
