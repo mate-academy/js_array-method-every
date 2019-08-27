f5c3f53504871a261588c0ad0aaafc1d2d1cf8e4
youvovk <wer3wollf@gmail.com>
2019-08-27 15:27:35 +0300

    delete unnecessary rules

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,15 +5,13 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    let valueEvery = true;
-
     for (let i = 0; i < this.length; i++) {
       if (!callback(this[i], i, this)) {
-        valueEvery = false;
+        return false;
       }
     }
 
-    return valueEvery;
+    return true;
   };
 }
 
