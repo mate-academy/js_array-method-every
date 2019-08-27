68c1051e25805cfdb997cc0d6a3a85e5d42b8723
Miroslava <777shadowpriest777@gmail.com>
2019-08-27 17:56:58 +0300

    refactored

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,20 +5,13 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    if (this.length === 0) {
-      return true;
-    }
-
-    let result = true;
-
     for (let i = 0; i < this.length; i++) {
       if (!callback(this[i], i, this)) {
-        result = false;
-        break;
+        return false;
       }
     }
 
-    return result;
+    return true;
   };
 }
 
