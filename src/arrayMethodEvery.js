77f6fcf15731a6e6ac9201c2b94001ec60547f37
lubremets <lubremets@gmail.com>
2019-10-04 18:54:21 +0300

    solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,13 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+      } else {
+        return false;
+      }
+    }
+    return true;
   };
 }
 
