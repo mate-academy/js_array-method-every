1f5071499b8e13989b94e3e8c182c5a03376f528
Viktor Lukovets <nuerio63@gmail.com>
2019-10-07 18:21:46 +0300

    Solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,12 +5,12 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    if (this.length === 0) {
-      return true;
-    }
     for (let i = 0; i < this.length; i++) {
-      return callback(this[i], i, this);
+      if (!callback(this[i], i, this)) {
+        return false;
+      }
     }
+    return true;
   };
 }
 
