e7042465f6fb44c1eba942f4f21709fcc54f6e04
Inna <inna.tereshchenko1@gmail.com>
2019-11-20 20:41:22 +0200

    solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,16 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+      const index = i;
+      const arr = this;
+      if (callback(item, index, arr) === true) {
+        continue;
+      }
+      return false;
+    }
+    return true;
   };
 }
 
