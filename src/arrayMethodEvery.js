12e2298969e8baa747b046dff8052afb80da1121
art1shok <kvitkaart554@gmail.com>
2019-11-21 11:05:56 +0200

    soution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -3,9 +3,18 @@
 /**
  * Implement method Every
  */
+
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    if (this.length === 0) {
+      return true;
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (!callback(this[i], i, this)) {
+        return false;
+      }
+    }
+    return true;
   };
 }
 
