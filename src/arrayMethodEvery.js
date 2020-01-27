09fcb3e77db9f152c61974a047f20e5c03e5b3ce
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-01-27 18:44:41 +0200

    solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,19 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    if (!arguments) {
+      return false;
+    }
+
+    let count = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        count++;
+      }
+    }
+
+    return count === this.length;
   };
 }
 
