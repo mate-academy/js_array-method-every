5b305c9ac1f807b22f2398e88146e4b211f6e62b
Maximka <maximkacmm@gmail.com>
2020-01-30 18:48:45 +0200

    fix commit

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,10 +5,6 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    if (this.length === 0) {
-      return true;
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (!callback(this[i], i, this)) {
         return false;
