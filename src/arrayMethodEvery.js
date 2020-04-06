01a157ad0656226889312a1d7fe0dc5b6e240760
Alex Machula <alexmachulamail@gmail.com>
2020-04-06 11:52:04 +0300

    solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,17 +5,13 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    if (this.length === 0) {
-      return true;
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-        return true;
-      } else {
+      if (!callback(this[i], i, this)) {
         return false;
       }
     }
+
+    return true;
   };
 }
 
