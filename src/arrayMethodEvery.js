bfdf4d62b5f162ae461d396fd52c101415152fb8
GryaznovMaksim <61284005+GryaznovMaksim@users.noreply.github.com>
2020-04-07 12:45:48 +0300

    Update src/arrayMethodEvery.js
    Co-Authored-By: Kryvytskyy Volodymyr <vovakrivickiy34@gmail.com>

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -6,8 +6,7 @@
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
     for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-      } else {
+      if (!callback(this[i], i, this)) {
         return false;
       }
     }
