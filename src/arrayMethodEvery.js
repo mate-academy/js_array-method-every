61fc7b827f1c788f85a7ca4e917d291465e7175a
Igor Kulish <igormagnetic@gmail.com>
2020-06-01 17:10:36 +0300

    solution

--- src/arrayMethodEvery.js
+++ src/arrayMethodEvery.js
@@ -5,7 +5,15 @@
  */
 function applyCustomEvery() {
   [].__proto__.every2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      const searchCondition = callback(this[i], i, this);
+
+      if (!searchCondition) {
+        return false;
+      }
+    }
+
+    return true;
   };
 }
 
